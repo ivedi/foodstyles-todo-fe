c8c0ebfb3a041188ecd76d20707cbae71ab73f55
ivedi <halil@veriyaz.com.tr>
2022-06-18 12:12:18 +0300

    created container

---
+++ components/PageContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import styled from 'styled-components/native';
+
+
+export default function PageContainer(props) {
+  return (
+    <Container>
+      {props.children}
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  width: 390px;
+  height: 530px;
+  padding: 35px 30px 63px;
+  border-radius: 8px;
+  background-color: white;
+
+  shadow-color: #000;
+  shadow-offset: { width: 16, height: 0 };
+  shadow-opacity: 0.1;
+  shadow-radius: 16;
+`;
